cd61644156eacdc32964699a9fb856979a632dd6
Isaac Hernandez <isaac.hho@gmail.com>
2014-01-30 20:59:18 -0500

    Basic Template
    Added a basic template for the index, books, about, and contacts pages.

---
+++ siteJS.js
@@ -0,0 +1,20 @@
+function writeMenu(){
+	document.write("<div class='menuBar'>");
+		document.write("<font size='10'>Scott MacDonald</font>");
+		document.write("<table align='center'><tr>");
+			document.write("<td><a href='index.html'>Home</a></td>");
+			document.write("<td>|</td>");
+			document.write("<td><a href='books.html'>Books</a></td>");
+			document.write("<td>|</td>");
+			document.write("<td><a href='about.html'>About</a></td>");
+			document.write("<td>|</td>");
+			document.write("<td><a href='contact.html'>Contact</a></td>");
+		document.write("</tr></table>");
+	document.write("</div>");
+}
+
+function writeFooter(){
+	document.write("<div class='footer'>");
+		document.write("Website by Adam Gwilliam & Isaac Hernandez.");
+	document.write("</div>")
+}
